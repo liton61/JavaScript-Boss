661c4c6dce465d249520f683f845e85113a37ea6
liton61 <litonofficial61@gmail.com>
2024-07-29 22:43:22 +0600

    shorthand

---
+++ shorthand.js
@@ -0,0 +1,19 @@
+// shorthand addition
+var price = 120;
+price += 30;
+console.log("Addition is : ", price);
+
+// shorthand subtraction
+var price = 120;
+price -= 20;
+console.log("Subtraction is : ", price);
+
+// multiplication
+var price = 5;
+price *= 6;
+console.log("Multiplication is : ", price);
+
+// division
+var price = 30;
+price /= 5;
+console.log("Division is : ", price);
