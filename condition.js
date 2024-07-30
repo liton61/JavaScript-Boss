db16093a49a00204fc39f61d86a3b4645df112af
liton61 <litonofficial61@gmail.com>
2024-07-30 23:02:58 +0600

    condition

---
+++ condition.js
@@ -0,0 +1,93 @@
+// condition
+var price = 120000;
+var budget = 90000;
+
+if (budget > price) {
+  //   console.log("I will by an iphone");
+} else {
+  //   console.log("I will buy a samsung");
+}
+
+// multiple condition
+var brand = "apple";
+var model = "15";
+var color = "purple";
+var room = "256";
+
+if (
+  brand === "apple" &&
+  model === "15" &&
+  color === "purple" &&
+  room === "512"
+) {
+  //   console.log("I will buy");
+} else {
+  //   console.log("I will not buy");
+}
+
+// multi stage condition
+var david = 85;
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var john = 40;
+
+if (david >= 80) {
+  console.log("David got A");
+} else if (david >= 60) {
+  console.log("David got B");
+} else if (david >= 50) {
+  console.log("David got C");
+} else if (david >= 40) {
+  console.log("David got D");
+} else {
+  console.log("David got F");
+}
+
+if (tom >= 80) {
+  console.log("Tom got A");
+} else if (tom >= 60) {
+  console.log("Tom got B");
+} else if (tom >= 50) {
+  console.log("Tom got C");
+} else if (tom >= 40) {
+  console.log("Tom got D");
+} else {
+  console.log("Tom got F");
+}
+
+if (jane >= 80) {
+  console.log("Jane got A");
+} else if (jane >= 60) {
+  console.log("Jane got B");
+} else if (jane >= 50) {
+  console.log("Jane got C");
+} else if (jane >= 40) {
+  console.log("Jane got D");
+} else {
+  console.log("Jane got F");
+}
+
+if (peter >= 80) {
+  console.log("Peter got A");
+} else if (peter >= 60) {
+  console.log("Peter got B");
+} else if (peter >= 50) {
+  console.log("Peter got C");
+} else if (peter >= 40) {
+  console.log("Peter got D");
+} else {
+  console.log("Peter got F");
+}
+
+if (john >= 80) {
+  console.log("John got A");
+} else if (john >= 60) {
+  console.log("John got B");
+} else if (john >= 50) {
+  console.log("John got C");
+} else if (john >= 40) {
+  console.log("John got D");
+} else {
+  console.log("John got F");
+}
